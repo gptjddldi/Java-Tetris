66215a878d07f678471ddb8375a03267f4dc55bb
kim400im <129593287+kim400im@users.noreply.github.com>
2024-03-28 22:00:22 +0900

    Delete my_tetris directory

--- my_tetris/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.myjavafxtetris {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.myjavafxtetris to javafx.fxml;
-    exports org.example.myjavafxtetris;
-}
