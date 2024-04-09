c99234fd60f4c1c76e9ff9b37fb90b09b45f6423
gptjddldi <hystom38@gmail.com>
2024-04-09 23:08:44 +0900

    add tetromino color

--- src/main/java/com/example/javatetris/TetrominoType.java
+++ src/main/java/com/example/javatetris/TetrominoType.java
@@ -1,21 +1,30 @@
 package com.example.javatetris;
 
+import javafx.scene.paint.Color;
+
 public enum TetrominoType {
-    I_SHAPE(new boolean[][]{{true, true, true, true}}),
-    J_SHAPE(new boolean[][]{{true, false, false}, {true, true, true}}),
-    L_SHAPE(new boolean[][]{{false, false, true}, {true, true, true}}),
-    O_SHAPE(new boolean[][]{{true, true}, {true, true}}),
-    S_SHAPE(new boolean[][]{{false, true, true}, {true, true, false}}),
-    T_SHAPE(new boolean[][]{{false, true, false}, {true, true, true}}),
-    Z_SHAPE(new boolean[][]{{true, true, false}, {false, true, true}});
+    I_SHAPE(new char[][]{{'O', 'O', 'O', 'O'}}, Color.CYAN),
+    J_SHAPE(new char[][]{{'O', 'N', 'N'}, {'O', 'O', 'O'}}, Color.BLUE),
+    L_SHAPE(new char[][]{{'N', 'N', 'O'}, {'O', 'O', 'O'}}, Color.ORANGE),
+    O_SHAPE(new char[][]{{'O', 'O'}, {'O', 'O'}}, Color.YELLOW),
+    S_SHAPE(new char[][]{{'N', 'O', 'O'}, {'O', 'O', 'N'}}, Color.GREEN),
+    T_SHAPE(new char[][]{{'N', 'O', 'N'}, {'O', 'O', 'O'}}, Color.PURPLE),
+    Z_SHAPE(new char[][]{{'O', 'O', 'N'}, {'N', 'O', 'O'}}, Color.RED);
 
-    private final boolean[][] shape;
 
-    TetrominoType(boolean[][] shape) {
+    private final char[][] shape;
+    private final Color color;
+
+    TetrominoType(char[][] shape, Color color) {
         this.shape = shape;
+        this.color = color;
     }
 
-    public boolean[][] getShape() {
+    public char[][] getShape() {
         return shape;
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
