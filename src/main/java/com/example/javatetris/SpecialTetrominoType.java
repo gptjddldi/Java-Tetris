ba70079e10df3806964fe3e531dd34fe743010d7
gptjddldi <hystom38@gmail.com>
2024-04-22 15:05:15 +0900

    add 5 items

--- src/main/java/com/example/javatetris/SpecialTetrominoType.java
+++ src/main/java/com/example/javatetris/SpecialTetrominoType.java
@@ -2,9 +2,8 @@ package com.example.javatetris;
 
 public enum SpecialTetrominoType implements TetrominoType {
     HEAVY_SHAPE,
-    HORIZON_SHAPE,
-    VERTICAL_SHAPE,
     BOMB_SHAPE,
     CROSS_SHAPE,
-
+    LINE_SHAPE,
+    VERTICAL_SHAPE,
 }
